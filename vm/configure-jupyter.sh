012131424fb0cf260fd1a6e3e430bd1acd96ca03
Lynn Root <lynn@lynnroot.com>
2022-11-03 13:17:03 -0400

    Revert back to port 80

--- vm/configure-jupyter.sh
+++ vm/configure-jupyter.sh
@@ -16,7 +16,7 @@ c.NotebookApp.allow_root = True
 ## The IP address the notebook server will listen on.
 c.NotebookApp.ip = '0.0.0.0'
 ## The port the notebook server will listen on.
-c.NotebookApp.port = 443
+c.NotebookApp.port = 80
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
 #  module, unless it is overridden using the --browser (NotebookApp.browser)
